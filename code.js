8c7e87d6a242064c81f57eafdc155adb123178d3
vixxer <83184947+menyaysya@users.noreply.github.com>
2022-10-04 20:19:35 +0300

    register-login

--- code.js
+++ code.js
@@ -1,1 +1,12 @@
-alert('hello Vova!');
+$('body').on('click', '.showpassword', function(){
+    if ($(this).is(':checked')){
+    $('#password-input').attr('type', 'text');
+    } else {
+    $('#password-input').attr('type', 'password');
+    }
+    if ($(this).is(':checked')){
+        $('#password-forgot').attr('type', 'text');
+        } else {
+        $('#password-forgot').attr('type', 'password');
+        }
+  });
